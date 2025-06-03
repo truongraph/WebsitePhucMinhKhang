d196dadb90eaa569907bec2968c3b2c306fe87b1
Võ Trường <truong.vd2000@gmail.com>
2025-06-03 16:32:55 +0700

    update last

--- wp-content/plugins/popup-maker/assets/sounds/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Popup Maker Silence is Golden.
- *
- * @package   PopupMaker
- * @copyright Copyright (c) 2024, Code Atlantic LLC
- */
--- wp-content/plugins/popup-maker/templates/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Silence is golden.
- *
- * @package   PopupMaker
- * @copyright Copyright (c) 2024, Code Atlantic LLC
- */
